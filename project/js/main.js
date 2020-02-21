9c0509bc6738ab21eadd2ccd650b7ffc4c37348c
jieyintan <51181712+jieyintan@users.noreply.github.com>
2020-02-21 21:20:13 +0800

    appid

--- project/js/main.js
+++ project/js/main.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
 })
 
 function getWeather() {
-	var url = "https://api.openweathermap.org/data/2.5/weather?q=London&APPID="+apiKey;
+	var url = "https://api.openweathermap.org/data/2.5/weather?q=London&appid="+apiKey;
 	$.ajax(url, {success: function(data) {
 		console.log(data);
 	}})

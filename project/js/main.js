46dd18392f4f4f6882d2489b931bf518be1ca888
jieyintan <51181712+jieyintan@users.noreply.github.com>
2020-02-22 23:11:40 +0800

    wrapper

--- project/js/main.js
+++ project/js/main.js
@@ -37,39 +37,45 @@
 // }
 
 
-$(document).ready(function (){
-	getPokemonA();
-	getPokemonB();
-})
+// $(document).ready(function (){
+// 	getPokemonA();
+// 	getPokemonB();
+// })
 
-function getPokemonA(){
-	var pokemonA = Math.floor((Math.random() * 151) + 1);
-	console.log(pokemonA);
-	var url ="https://pokeapi.co//api/v2/pokemon/"+pokemonA+"/";
-	$.ajax(url, {
-		success: function(data) {
-			$(".nameA").text("data.name");
-		},
-		error: function(error){
-			$(".error-message").text("An error occured");
-		}
-	})
-}
+// function getPokemonA(){
+// 	var pokemonA = Math.floor((Math.random() * 151) + 1);
+// 	console.log(pokemonA);
+// 	var url ="https://pokeapi.co//api/v2/pokemon/"+pokemonA+"/";
+// 	$.ajax(url, {
+// 		success: function(data) {
+// 			$(".nameA").text("data.name");
+// 		},
+// 		error: function(error){
+// 			$(".error-message").text("An error occured");
+// 		}
+// 	})
+// }
 
-function getPokemonB(){
-	var pokemonB = Math.floor((Math.random() * 151) + 1);
-	var url ="https://pokeapi.co//api/v2/pokemon/"+pokemonB"/";
-	$.ajax(url, {
-		success: function(data) {
-			$(".nameB").text(data.name);
-		},
-		error: function(error){
-			$(".error-message").text("An error occured");
-		}
-	})
-}
+// function getPokemonB(){
+// 	var pokemonB = Math.floor((Math.random() * 151) + 1);
+// 	var url ="https://pokeapi.co//api/v2/pokemon/"+pokemonB"/";
+// 	$.ajax(url, {
+// 		success: function(data) {
+// 			$(".nameB").text(data.name);
+// 		},
+// 		error: function(error){
+// 			$(".error-message").text("An error occured");
+// 		}
+// 	})
+// }
 
+const Pokedex = require('pokeapi-js-wrapper');
+const P = new Pokedex.Pokedex();
 
+P.getPokemonByName('eevee') // with Promise
+    .then(function(response) {
+      console.log(response);
+    });
 
 
 

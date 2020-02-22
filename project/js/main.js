7c46a1231fb4b9f5f647a3807fb09ee4b2e92ed7
jieyintan <51181712+jieyintan@users.noreply.github.com>
2020-02-22 16:27:40 +0800

    pokemon

--- project/js/main.js
+++ project/js/main.js
@@ -1,37 +1,50 @@
-$(document).ready(function(){
-	getWeather();
-})
+// $(document).ready(function(){
+// 	getWeather();
+// })
 
-function getWeather(searchQuery) {
-	var url = "https://api.openweathermap.org/data/2.5/weather?q="+searchQuery+"&appid="+apiKey;
+// function getWeather(searchQuery) {
+// 	var url = "https://api.openweathermap.org/data/2.5/weather?q="+searchQuery+"&appid="+apiKey;
 	
-	$(".city").text("");
-	$(".temp").text("");
-	$(".error-message").text("");
+// 	$(".city").text("");
+// 	$(".temp").text("");
+// 	$(".error-message").text("");
 
 
-	$.ajax(url, {success: function(data) {
-		$(".city").text(data.name);
-		$(".temp").text(data.main.temp);
-	}, error: function(error){
-		$(".error-message").text("An error occured");
-	}
-})
-}
+// 	$.ajax(url, {success: function(data) {
+// 		$(".city").text(data.name);
+// 		$(".temp").text(data.main.temp);
+// 	}, error: function(error){
+// 		$(".error-message").text("An error occured");
+// 	}
+// })
+// }
 
-function searchWeather() {
-	var searchQuery = $(".search").val();
-	getWeather(searchQuery);
-}
+// function searchWeather() {
+// 	var searchQuery = $(".search").val();
+// 	getWeather(searchQuery);
+// }
 
 
 
-function showPicture(){
-  // use jQuery ($ is shorthand) to find the div on the page and then change the html
-  // 'rounded-circle' is a bootstrap thing! Check out more here: http://getbootstrap.com/css/
-  $("#image").append('<img class="rounded-circle" src="images/high-five.gif"/>');
-  $("p").html("High five! You're building your first web app!");
+// function showPicture(){
+//   // use jQuery ($ is shorthand) to find the div on the page and then change the html
+//   // 'rounded-circle' is a bootstrap thing! Check out more here: http://getbootstrap.com/css/
+//   $("#image").append('<img class="rounded-circle" src="images/high-five.gif"/>');
+//   $("p").html("High five! You're building your first web app!");
 
-  // jQuery can do a lot of crazy stuff, so make sure to Google around to find out more
+//   // jQuery can do a lot of crazy stuff, so make sure to Google around to find out more
   
+// }
+
+$document.ready(function (){
+	getPokemonA();
+	getPokemonB();
+})
+
+function getPokemonA(){
+	Math.floor((Math.random() * 151) + 1);
+}
+
+function getPokemonB(){
+	Math.floor((Math.random() * 151) + 1);
 }

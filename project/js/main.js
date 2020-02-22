78612274d6f6002643806c5239d964d5f8d99429
jieyintan <51181712+jieyintan@users.noreply.github.com>
2020-02-22 16:47:03 +0800

    test

--- project/js/main.js
+++ project/js/main.js
@@ -36,6 +36,17 @@
   
 // }
 
+var Pokedex = require('pokedex-promise-v2');
+var P = new Pokedex();
+
+ P.getPokemonByName(34, function(response, error) { // with callback
+      if(!error) {
+        console.log(response);
+      } else {
+        console.log(error)
+      }
+    });
+
 $(document).ready(function (){
 	getPokemonA();
 	getPokemonB();
